68e292735f4180e0b823ae982b3c12c3492af550
Ahmad Ghallab <help.ahmadghallab@gmail.com>
2020-01-16 14:54:25 +0200

    Change Welcome Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,18 +81,16 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#">Home</a>
+                    <a href="#">How it works</a>
+                    <a href="#">Docs</a>
+                    <a href="#">Support</a>
+                    <a href="#">Admin</a>
+                    <a href="https://github.com/ahmadghallab/trans-app">GitHub Repo</a>
                 </div>
             </div>
         </div>
