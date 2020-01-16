18d651e8c6b13d7eb95bd8e8071085aa95dc8621
Ahmad Ghallab <help.ahmadghallab@gmail.com>
2020-01-16 09:21:42 +0200

    Initial commit

---
+++ database/migrations/2020_01_16_071217_create_stations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->unique();
+            $table->decimal('latitudes', 10, 8);
+            $table->decimal('longitude', 11, 8);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stations');
+    }
+}
